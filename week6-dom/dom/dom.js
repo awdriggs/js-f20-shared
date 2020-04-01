902f6220e1f2c98d32e35371a19736d959c9144c
adam driggers <awdriggs@gmail.com>
2020-04-01 19:36:21 -0400

    dom 1

--- week6-dom/dom/dom.js
+++ week6-dom/dom/dom.js
@@ -2,6 +2,28 @@
 //i'm going to put all the code I want to run in here
 //why is this needed??
 window.onload = function() {
+  // selecting using the tag name
+  // document.querySelector("h1").innerHTML = "Adam's Todo List"
 
+  document.querySelector("#title").innerHTML = "Adam's Todo List"
 
+  //select using the clas name
+  var list = document.querySelector(".todoList");
+
+  //creates new list item
+  var todo = document.createElement('li');
+  //adds content to the new list item
+  todo.innerHTML = "Cook Dinner";
+
+  //add to the parent
+  list.append(todo);
+
+  var todo2 = document.createElement('li');
+  todo2.innerHTML = "Clean Kitchen";
+  list.append(todo2);
+
+  //adds inline style
+  //todo2.style.color ="orange";
+  todo2.classList.add("current");
+  debugger;
 }
