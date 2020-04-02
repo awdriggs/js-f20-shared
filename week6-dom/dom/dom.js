f452e46ed35bf78d05bbdd7f2fe613026d573c43
adam driggers <awdriggs@gmail.com>
2020-04-01 20:25:51 -0400

    dom2

--- week6-dom/dom/dom.js
+++ week6-dom/dom/dom.js
@@ -27,5 +27,29 @@ window.onload = function() {
   //adds inline style
   //todo2.style.color ="orange";
   todo2.classList.add("current");
-  debugger;
+
+  //create an event listener
+  var add = document.querySelector('#add');
+  add.addEventListener("click", addItem);
+}
+
+var addItem = function() {
+  //store the text in the text box
+  var textInput = document.querySelector('#newItem')
+  var newListItem = textInput.value
+  //creates new list item
+  var todo = document.createElement('li');
+  //adds content to the new list item
+  todo.innerHTML = newListItem;
+
+
+  //select using the clas name
+  var list = document.querySelector(".todoList");
+  //add to the parent
+  list.append(todo);
+
+  textInput.value = "";
+  // document.querySelector('#newItem').value = " "
+
+
 }
--- week6-dom/spoofify/app.js
+++ week6-dom/spoofify/app.js
@@ -7,10 +7,12 @@ window.onload = function() {
 
   // Add the class 'now-playing' to the first song you added
 
-  //PART2 
+  //PART2
   // Return the the playlist exercise.
   // Use “for...of” to make the songs you added previously have a class of 'played'.
 
   // Use a for loop to add 5 more songs. You can make the names generic, like “new song 1”
+
+
 
 }
